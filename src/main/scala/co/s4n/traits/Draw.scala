e1f92c364905d5b264c19cc35da36a894145ec0e
johan <johanvargas@seven4n.com>
2021-03-03 19:53:20 -0500

    Arreglé la función apply

--- src/main/scala/co/s4n/traits/Draw.scala
+++ src/main/scala/co/s4n/traits/Draw.scala
@@ -3,12 +3,16 @@ package co.s4n.traits
 /**
  * Objeto draw
  */
-object Draw extends App {
+object Draw {
   /**
    * Método apply recibe una forma y obtiene el tostring dependiendo del tipo de forma
    *
    * @param forma Forma a recibir
    * @return Metodo to string que se deriva del tipo de forma
    */
-  def apply(forma: Forma): String = forma.toString
+  def apply(forma: Forma): Forma = forma match {
+    case Circulo(_) => forma
+    case Cuadrado(_) => forma
+    case Rectangulo(_, _) => forma
+  }
 }
--- src/test/scala/co/s4n/traits/FormaSpec.scala
+++ src/test/scala/co/s4n/traits/FormaSpec.scala
@@ -7,12 +7,12 @@ class FormaSpec extends AnyFlatSpec with Matchers {
 
   "1. Draw(new Círculo(10) " should " Un círculo de radio 10.0 " in {
     val circulo = Draw(Circulo(10))
-    circulo shouldBe "Un círculo de ancho 10.0"
+    circulo.toString shouldBe "Un círculo de radio 10.0"
   }
 
   "2. Draw(new Rectangulo(3,4) " should " Un rectangulo de ancho 3.0cm y largo 4.0cm " in {
     val rectangulo = Draw(Rectangulo(3, 4))
-    rectangulo shouldBe "Un rectángulo de ancho 3.0cm y largo 4.0cm"
+    rectangulo.toString shouldBe "Un rectángulo de ancho 3.0cm y largo 4.0cm"
   }
 
 }
